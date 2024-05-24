9aa7aafd24d73f646139b94e6b992f0c59dc184b
ajey108 <ajayxa1@gmail.com>
2024-05-24 20:47:37 +0530

    updated search bar

--- src/Search.jsx
+++ src/Search.jsx
@@ -8,12 +8,12 @@ const Search = () => {
     <>
       <div className="fixed top-0 left-0 w-full bg-white shadow z-10">
         <form className="text-center" action="#" onSubmit={(e) => e.preventDefault()}>
-          <div className="flex justify-center items-center m-2 p-3">
+          <div className="flex justify-center items-center m-2 p-2 md:p-3">
             <label htmlFor="movieSearch" className="sr-only">Search Movie</label>
-            <span className="text-4xl font-thin text-yellow-500 mr-4">IMDB</span>
+            <span className="text-xl md:text-4xl font-thin text-yellow-500 mr-2 md:mr-4">IMDB</span>
             <input
               id="movieSearch"
-              className="rounded-lg border border-gray-300 focus:border-blue-500 focus:outline-none w-full md:w-64 px-4 py-2"
+              className="rounded-lg border border-gray-300 focus:border-blue-500 focus:outline-none w-full md:w-64 px-2 md:px-4 py-1 md:py-2 text-sm md:text-base"
               type="text"
               placeholder="Search movie"
               value={search}
@@ -22,10 +22,10 @@ const Search = () => {
           </div>
         </form>
         {Error.show && (
-          <p className="text-center m-2 text-gray-500 text-lg">{Error.msg}</p>
+          <p className="text-center m-2 text-gray-500 text-sm md:text-lg">{Error.msg}</p>
         )}
       </div>
-      <div className="h-20"></div> {/* Placeholder to prevent content from being obscured by fixed navbar */}
+      <div className="h-16 md:h-20"></div> {/* Placeholder to prevent content from being obscured by fixed navbar */}
     </>
   );
 };
